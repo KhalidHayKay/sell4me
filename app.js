b1a36536300c6c73811cbf1aa9567532e18c2e33
Alamutu Khalid <alamutukhalid@gmail.com>
2023-06-17 03:12:29 -0900

    responsiveness commit 3

--- app.js
+++ app.js
@@ -14,45 +14,71 @@ const loginDialog = document.getElementById('login-dialog');
 const loginBtn = document.querySelectorAll('.login>a')[1];
 const resetDialog = document.getElementById('reset-dialog');
 
+function scrollDeactivation(){
+    const x = window.scrollX;
+    const y = window.scrollY;
+    window.onscroll = function() {window.scrollTo(x, y);}
+}
+function scrollReactivation(){
+    window.onscroll = function(){}
+}
+
+/////////   FOR SIGNUP MODAL
 signupBtn.addEventListener('click', e => {
     menu.classList.remove('animate');
     hamburger.classList.remove('animate');
     signupDialog.show();
-
-    document.querySelector('#signup-dialog>button')
-    .addEventListener('click', e => {
-        signupDialog.close();
-    })
+    scrollDeactivation()
 })
+document.querySelector('#signup-dialog button')
+.addEventListener('click', e => {
+    signupDialog.close();
+    scrollReactivation();
+})
+document.querySelector('#signup-dialog p>a')
+.addEventListener('click', e => {
+    signupDialog.close();
+    loginDialog.show();
+})
+
+///////// FOR LOGIN MODAL
 loginBtn.addEventListener('click', e => {
     menu.classList.remove('animate');
     hamburger.classList.remove('animate');
     loginDialog.show();
-
-    document.querySelector('#login-dialog>button')
-    .addEventListener('click', e => {
-        loginDialog.close();
-    })
-
-    document.querySelector('.forgot-password')
-    .addEventListener('click', e => {
-        loginDialog.close();
-        resetDialog.show();
-
-        document.querySelector('#reset-dialog a')
-        .addEventListener('click', e => {
-            resetDialog.close();
-            loginDialog.show();
-        })
-    })
+    scrollDeactivation();
 })
-
-// console.log(document.querySelector('#login-dialog p>a'))
-document.querySelector('#login-dialog p>a').addEventListener('click', e => {
+document.querySelector('#login-dialog button')
+.addEventListener('click', e => {
+    loginDialog.close();
+    scrollReactivation();
+})
+document.querySelector('#login-dialog p>a')
+.addEventListener('click', e => {
     loginDialog.close();
     signupDialog.show();
+    scrollDeactivation();
 })
-document.querySelector('#signup-dialog p>a').addEventListener('click', e => {
-    signupDialog.close();
-    loginDialog.show();
+
+//////  FOR PASSWOED RESET MODAL
+document.querySelector('.forgot-password')
+.addEventListener('click', e => {
+    loginDialog.close();
+    resetDialog.show();
+    scrollDeactivation();
+})
+document.querySelector('#reset-dialog a')
+    .addEventListener('click', e => {
+        resetDialog.close();
+        loginDialog.show();
+        scrollDeactivation();
+})
+
+////////////
+document.querySelector('.how-it-works a')
+.addEventListener('click', e => {
+    menu.classList.remove('animate');
+    hamburger.classList.remove('animate');
+    signupDialog.show();
+    scrollDeactivation()
 })
